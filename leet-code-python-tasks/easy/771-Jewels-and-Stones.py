3bbe8b5d07bfe09c4537b4a73fbabe4876dc99f8
ilya_krotov <ilyakrotov58@gmail.com>
2025-04-09 21:58:04 +0400

    improvement

--- leet-code-python-tasks/easy/771-Jewels-and-Stones.py
+++ leet-code-python-tasks/easy/771-Jewels-and-Stones.py
@@ -3,16 +3,12 @@
 # Each character in stones is a type of stone you have.
 # You want to know how many of the stones you have are also jewels.
 #
-# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+# Letters are case-sensitive, so "a" is considered a different type of stone from "A".
 
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        result = 0
         jewels_set = set(jewels)
-        for i in stones:
-            if i in jewels_set:
-                result += 1
-        return result
+        return len([stone for stone in stones if stone in jewels_set])
 
 sol = Solution()
 
