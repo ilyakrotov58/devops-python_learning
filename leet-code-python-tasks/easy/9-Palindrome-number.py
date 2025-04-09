714891d04b0fc251c326119803625196629a2576
ilya_krotov <ilyakrotov58@gmail.com>
2025-04-09 21:59:56 +0400

    improvement

--- leet-code-python-tasks/easy/9-Palindrome-number.py
+++ leet-code-python-tasks/easy/9-Palindrome-number.py
@@ -3,10 +3,7 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if str(x) == (str(x)[::-1]):
-            return True
-        else:
-            return False
+        return str(x) == (str(x)[::-1])
 
 sol = Solution()
 
